0c529e38036264c74db3b683882ba3bdb9805176
Jeff Espenschied <jeffrey.espenschied@datarobot.com>
2023-11-14 16:40:43 -0500

    More changes. Working app now.

--- main.py
+++ main.py
@@ -1,99 +1,138 @@
 from contextlib import contextmanager
 from fastapi import FastAPI, BackgroundTasks, HTTPException
 from pydantic import BaseModel
-from typing import Optional, List
+from typing import Dict, Optional, List
 import uuid
 import time
 import duckdb
-
-@contextmanager
-def _get_db(application_id='test-app'):
-    db = duckdb.connect()
-    try:
-        db.sql(f"ATTACH '{application_id}'")
-        db.sql("PRAGMA memory_limit='16GB'")
-        db.sql("PRAGMA threads=4")
-        yield db
-    finally:
-        db.close()
 
 app = FastAPI()
 
 datasets = {}
 views = {}
-tasks = {}  # Task status tracking
+tasks = {}
+
 
 class Dataset(BaseModel):
-    data: List[List[str]]
+    sources: List[str]
+    source_type: str = 'csv'
+    connection_attr: Dict[str, str]
+
 
 class Query(BaseModel):
     dataset_id: str
+    cache_result: bool = False
     query: str
+
 
 class ViewQuery(BaseModel):
     dataset: List[List[str]]
     query: str
 
 
-def process_dataset(dataset_id: str, dataset: List[List[str]]):
+@contextmanager
+def get_db(application_id='duck-db'):
+    db = duckdb.connect()
     try:
-        tasks[dataset_id] = "processing"
-        time.sleep(10)  # Simulating a long-running task
-        datasets[dataset_id] = dataset
-        tasks[dataset_id] = "completed"
-    except Exception as e:
-        tasks[dataset_id] = f"failed: {str(e)}"
+        db.sql(f"ATTACH '{application_id}'")
+        db.sql(f"USE '{application_id}'")
+        db.sql("PRAGMA memory_limit='16GB'")
+        db.sql("PRAGMA threads=4")
+        yield db
+    finally:
+        db.close()
 
-def process_query(view_id: str, dataset_id: str, query: str):
+
+def format_source(dataset: Dataset):
+    if dataset.source_type == 'csv':
+        return f"read_csv_auto({dataset.sources}, header=true)"
+    elif dataset.source_type == 'parquet':
+        return f"read_parquet({dataset.sources})"
+    else:
+        raise HTTPException(status_code=400, detail="Must provide a source_type")
+
+
+def process_dataset(task_id: str, dataset_id: str, dataset: Dataset):
     try:
-        tasks[view_id] = "processing"
-        time.sleep(5)  # Simulating a long-running task
-        views[view_id] = datasets[dataset_id]  # Mock view creation
-        tasks[view_id] = "completed"
+        tasks[task_id] = "processing"
+        with get_db() as db:
+            # using region as indication that all are present.
+            if 's3-region' in dataset.connection_attr:
+                db.sql(f"SET s3_region='{dataset.connection_attr['s3-region']}'")
+                db.sql(f"SET s3_access_key_id='{dataset.connection_attr['s3-access-key']}'")
+                db.sql(f"SET s3_secret_access_key='{dataset.connection_attr['s3-secret']}'")
+                db.sql(f"SET s3_session_token='{dataset.connection_attr['s3-session']}'")
+            db.sql(f'CREATE TABLE "{dataset_id}" AS SELECT * FROM {format_source(dataset)}')
+        datasets[dataset_id] = dataset
+        tasks[task_id] = "completed"
     except Exception as e:
-        tasks[view_id] = f"failed: {str(e)}"
+        tasks[task_id] = f"failed: {str(e)}"
+
+
+def process_query(task_id: str, view_id: str, cache_result: bool, query: str):
+    try:
+        tasks[task_id] = "processing"
+        with get_db() as db:
+            if cache_result:
+                db.sql(f'CREATE TABLE "{view_id}" AS ({query})')
+            else:
+                db.sql(f'CREATE VIEW "{view_id}" AS ({query})')
+        views[view_id] = {}
+        tasks[task_id] = "completed"
+    except Exception as e:
+        tasks[task_id] = f"failed: {str(e)}"
+
 
 @app.post("/register-dataset")
 def register_dataset(dataset: Dataset, background_tasks: BackgroundTasks):
     dataset_id = str(uuid.uuid4().hex)
-    background_tasks.add_task(process_dataset, dataset_id, dataset.data)
+    task_id = str(uuid.uuid4().hex)
+    background_tasks.add_task(process_dataset, task_id, dataset_id, dataset)
     tasks[dataset_id] = "queued"
-    return {"dataset_id": dataset_id, "message": "Dataset registration in progress"}
+    return {
+        "task_id": task_id,
+        "dataset_id": dataset_id,
+        "message": "Dataset registration in progress",
+    }
+
 
 @app.post("/execute-query")
 def execute_query(query: Query, background_tasks: BackgroundTasks):
     if query.dataset_id not in datasets:
         raise HTTPException(status_code=404, detail="Dataset not found")
-    
-    view_id = str(uuid.uuid4().hex)
-    background_tasks.add_task(process_query, view_id, query.dataset_id, query.query)
-    tasks[view_id] = "queued"
-    return {"view_id": view_id, "message": "Query execution in progress"}
 
-@app.post("/create-temporary-view")
-def create_temporary_view(query: ViewQuery):
-    view_id = str(len(views) + 1)
-    views[view_id] = query.dataset  # Creating a view from the given dataset
-    return {"view_id": view_id}
+    view_id = str(uuid.uuid4().hex)
+    task_id = str(uuid.uuid4().hex)
+
+    background_tasks.add_task(process_query, task_id, view_id, query.cache_result, query.query)
+    tasks[task_id] = "queued"
+    return {"task_id": task_id, "view_id": view_id, "message": "Query execution in progress"}
+
 
 @app.get("/query-results")
-def query_results(view_id: Optional[str] = None, dataset_id: Optional[str] = None):
+def query_results(
+    view_id: Optional[str] = None,
+    dataset_id: Optional[str] = None,
+    page_index: int = 0,
+    page_size: int = 100,
+):
     if view_id:
-        if view_id in views:
-            return views[view_id]
-        else:
-            raise HTTPException(status_code=404, detail="View not found")
+        id = view_id
     elif dataset_id:
-        if dataset_id in datasets:
-            return datasets[dataset_id]
-        else:
-            raise HTTPException(status_code=404, detail="Dataset not found")
+        id = dataset_id
     else:
         raise HTTPException(status_code=400, detail="View ID or Dataset ID required")
+
+    with get_db() as db:
+        db.execute(f'SELECT * FROM "{id}" OFFSET {page_index * page_size} LIMIT {page_size}')
+        data = db.fetchall()
+        db.execute(f'SELECT count(*) FROM "{id}"')
+        count = db.fetchone()
+
+    return {"data": data, "totalCount": count, "pageIndex": page_index, "pageSize": page_size}
+
 
 @app.get("/task-status/{task_id}")
 def task_status(task_id: str):
     status = tasks.get(task_id, "unknown")
     return {"task_id": task_id, "status": status}
-
-
